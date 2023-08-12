205e9cd540e1003bef872140818caa61b94ce981
Marc Nadim Zeenny <84089275+marcnz06@users.noreply.github.com>
2023-08-12 12:05:06 +0300

    minor update - test

--- artax/urls.py
+++ artax/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("faq/", views.faq, name="faq"),
-    path("contact/", views.contact, name="contact"),
+    path("contact/", views.contact, name="contact") ,
     path("blank/", views.blank, name="blank"),
     path("login/", views.login_view, name="login"),
     path("register/", views.new_user, name="register"),
